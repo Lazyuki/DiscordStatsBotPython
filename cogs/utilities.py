52ce5955b2017fb0fce44c8933bc66428fb9996c
Yuki Ito <yukiito@uw.edu>
2020-04-14 20:35:24 -0400

    ban notif

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from datetime import datetime
 import discord
 import logging
+import asyncio 
 
 BOOSTER_COLOR = 0xf47fff
 
@@ -20,6 +21,29 @@ class Utilities(commands.Cog):
         embed.timestamp = datetime.utcnow()
         embed.set_footer(text=f'Nitro Boosts: {ctx.guild.premium_subscription_count} (Tier {ctx.guild.premium_tier})')
         await ctx.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+        """Notify ban"""
+        # Fetch audit log to get who banned them
+        banner = None
+        reason = None
+        await asyncio.sleep(1)
+        async for entry in guild.audit_logs(action=discord.AuditLogAction.ban):
+            if entry.target.id == user.id:
+                banner = entry.user
+                reason = entry.reason
+                break
+        
+        embed = discord.Embed(colour=0x000000)
+        embed.title = f'\N{CROSS MARK} **{user.name}#{user.discriminator}** was `banned`. ({user.id})'
+        embed.description = f'*by* {banner if banner else "Unknown"}\n**Reason:** {reason if reason else "Unknown"}'
+        embed.timestamp = datetime.utcnow()
+        embed.set_footer(text=f'User Banned', icon_url=user.avatar_url_as(static_format='png'))
+        chan = guild.get_channel(self.settings[guild.id].log_channel_id)
+        if chan:
+            await chan.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
