489fed209a1f02d19ebe8ed91b04510562da5204
Yuki Ito <yukiito@uw.edu>
2020-04-20 19:43:32 -0400

    kick notif

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -45,6 +45,29 @@ class Utilities(commands.Cog):
         if chan:
             await chan.send(embed=embed)
 
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        """Notify kick"""
+        # Fetch audit log to get who banned them
+        kicker = None
+        reason = None
+        guild = member.guild
+        await asyncio.sleep(1)
+        async for entry in guild.audit_logs(action=discord.AuditLogAction.kick):
+            if entry.target.id == member.id:
+                kicker = entry.user
+                reason = entry.reason
+                break
+        else:
+            return
+        log.info(f'Kicked by {kicker.name if kicker else "Unknown"}')
+        embed = discord.Embed(colour=0x000000)
+        embed.description = f'\N{CROSS MARK} **{member.name}#{member.discriminator}** was `kicked`. ({member.id})\n\n*by* {kicker.mention if kicker else "Unknown"}\n**Reason:** {reason if reason else "Unknown"}'
+        embed.timestamp = datetime.utcnow()
+        embed.set_footer(text=f'User Kicked', icon_url=member.avatar_url_as(static_format='png'))
+        chan = guild.get_channel(self.settings[guild.id].log_channel_id)
+        if chan:
+            await chan.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
